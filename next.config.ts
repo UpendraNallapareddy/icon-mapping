f64e947761ac978c645824f9a09ac9276fe03eba
Upendra N <upendra.nallapareddy@gmail.com>
2025-09-05 08:01:33 +0900

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbopackScopeHoisting: false,
+  },
 };
 
 export default nextConfig;
