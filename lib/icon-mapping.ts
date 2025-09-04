58e29ebcd5d7b0a5bae027c56a935c753cddbc56
Upendra N <upendra.nallapareddy@gmail.com>
2025-09-04 22:38:00 +0900

    initial

---
+++ lib/icon-mapping.ts
@@ -0,0 +1,66 @@
+// Icon mapping from MUI to Lucide
+export const iconMapping = {
+  // Basic actions
+  Add: "Plus",
+  Error: "AlertCircle",
+  ErrorOutline: "AlertCircle",
+  ErrorOutlineRounded: "AlertCircle",
+  Warning: "AlertTriangle",
+  CheckCircleOutline: "CheckCircle",
+  CheckCircleOutlineOutlined: "CheckCircle",
+
+  // Navigation
+  ChevronLeft: "ChevronLeft",
+  ChevronRight: "ChevronRight",
+  ExpandMore: "ChevronDown",
+  ExpandMoreOutlined: "ChevronDown",
+  ExpandLessOutlined: "ChevronUp",
+  KeyboardArrowDown: "ChevronDown",
+  KeyboardArrowUp: "ChevronUp",
+  West: "ArrowLeft",
+
+  // Circle icons
+  ArrowCircleLeftOutlined: "ArrowLeft",
+  ArrowCircleRightOutlined: "ArrowRight",
+  AddCircleOutline: "PlusCircle",
+  RemoveCircleOutline: "MinusCircle",
+  CancelOutlined: "X",
+
+  // Interface elements
+  MoreVert: "MoreVertical",
+  DeleteOutline: "Trash2",
+  EditOutlined: "Edit",
+  DragIndicator: "GripVertical",
+  FilterList: "Filter",
+  Tune: "SlidersHorizontal",
+
+  // Content
+  TextFields: "Type",
+  FolderOutlined: "Folder",
+  ImageOutlined: "Image",
+  ChatBubble: "MessageCircle",
+  ChatBubbleOutline: "MessageCircle",
+  FormatListBulleted: "List",
+
+  // Settings and controls
+  Settings: "Settings",
+  SettingsOutlined: "Settings",
+  HorizontalRule: "Minus",
+  Replay: "RotateCcw",
+
+  // Security and status
+  HttpsOutlined: "Lock",
+  PublicOff: "EyeOff",
+  BlockOutlined: "Ban",
+
+  // Actions
+  Search: "Search",
+  Send: "Send",
+  Logout: "LogOut",
+  OpenInNew: "ExternalLink",
+  Home: "Home",
+  HomeOutlined: "Home",
+
+  // Special cases
+  HdrAutoOutlined: "Radio",
+};
